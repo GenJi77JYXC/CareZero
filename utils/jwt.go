ce881f834f34377d929339ca23b32a148bf7f9e6
GenJi <genji77@qq.com>
2025-01-24 20:12:26 +0800

    添加续期token的方法RefreshAccessToken

--- utils/jwt.go
+++ utils/jwt.go
@@ -6,9 +6,11 @@ import (
 	"encoding/base64"
 	"errors"
 	"github.com/golang-jwt/jwt/v4"
+	"github.com/zeromicro/go-zero/core/logx"
 	"github.com/zeromicro/go-zero/core/stores/redis"
 	"strconv"
 	"time"
+	"www.genji.xin/backend/CareZero/model"
 )
 
 const (
@@ -17,25 +19,28 @@ const (
 	RefreshExpire = time.Hour * 144
 )
 
-func GenerateToken(userId uint, role string, rds *redis.Redis) (string, error) {
+func GenerateToken(userId uint, rds *redis.Redis) (string, error) {
+
 	refreshToken, err := generateRefreshToken()
 	if err != nil {
+		logx.Error("生成RefreshToken错误:", err)
 		return "", err
 	}
 	claims := jwt.MapClaims{
 		"user_id":     userId,
-		"role":        role,
 		"refresh":     refreshToken,
 		"refresh_exp": time.Now().Add(RefreshExpire).Unix(),
 		"access_exp":  time.Now().Add(AccessExpire).Unix(), // 设置过期时间
 	}
-	token, err := jwt.NewWithClaims(jwt.SigningMethodHS256, claims).SignedString(jwtSecret)
+	token, err := jwt.NewWithClaims(jwt.SigningMethodHS256, claims).SignedString([]byte(jwtSecret))
 	if err != nil {
+		logx.Error("签发Token错误:", err)
 		return "", err
 	}
 
-	err = storeRefreshTokenInRedis(rds, token, int64(userId), time.Now().Add(RefreshExpire).Unix())
+	err = storeRefreshTokenInRedis(rds, refreshToken, int64(userId), time.Now().Add(RefreshExpire).Unix())
 	if err != nil {
+		logx.Error("存储refreshToken错误", err)
 		return "", err
 	}
 
@@ -69,22 +74,22 @@ func generateRefreshToken() (string, error) {
 	return base64.URLEncoding.EncodeToString(b), nil
 }
 
-func storeRefreshTokenInRedis(rdb *redis.Redis, token string, userID int64, expiresAt int64) error {
+func storeRefreshTokenInRedis(rdb *redis.Redis, RefreshToken string, userID int64, expiresAt int64) error {
 	ctx := context.Background()
-	err := rdb.SetCtx(ctx, token, strconv.FormatInt(userID, 10))
+	err := rdb.SetCtx(ctx, RefreshToken, strconv.FormatInt(userID, 10))
 	if err != nil {
 		return err
 	}
 
-	err = rdb.ExpireCtx(ctx, token, int(time.Unix(expiresAt, 0).Sub(time.Now()).Seconds()))
+	err = rdb.ExpireCtx(ctx, RefreshToken, int(time.Unix(expiresAt, 0).Sub(time.Now()).Seconds()))
 	if err != nil {
 		return err
 	}
 	return err
 }
 
-func validateRefreshToken(rdb *redis.Redis, token string) (int, error) {
-	userID, err := rdb.Get(token)
+func validateRefreshToken(rdb *redis.Redis, RefreshToken string) (int, error) {
+	userID, err := rdb.Get(RefreshToken)
 	if err != nil {
 		return 0, err
 	}
@@ -93,4 +98,16 @@ func validateRefreshToken(rdb *redis.Redis, token string) (int, error) {
 		return 0, err
 	}
 	return id, nil
+}
+
+func RefreshAccessToken(rds *redis.Redis, refreshToken string) (string, error) {
+	id, err := validateRefreshToken(rds, refreshToken)
+	if err != nil {
+		return "", err
+	}
+	if id == 0 {
+		return "", errors.New(model.ErrorMsg[model.TokenInvalid])
+	}
+
+	return GenerateToken(uint(id), rds)
 }
