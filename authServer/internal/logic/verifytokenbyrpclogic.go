94346fea98279ffc4334fb77a1876546a0b5a4ae
GenJi <genji77@qq.com>
2025-01-25 23:58:04 +0800

    完善校验Token的逻辑，先判断是否注销

--- authServer/internal/logic/verifytokenbyrpclogic.go
+++ authServer/internal/logic/verifytokenbyrpclogic.go
@@ -2,6 +2,9 @@ package logic
 
 import (
 	"context"
+	"fmt"
+	"google.golang.org/protobuf/types/known/structpb"
+	"www.genji.xin/backend/CareZero/model"
 	"www.genji.xin/backend/CareZero/utils"
 
 	"www.genji.xin/backend/CareZero/authServer/auth"
@@ -25,13 +28,30 @@ func NewVerifyTokenByRPCLogic(ctx context.Context, svcCtx *svc.ServiceContext) *
 }
 
 func (l *VerifyTokenByRPCLogic) VerifyTokenByRPC(in *auth.VerifyTokenReq) (*auth.VerifyResp, error) {
-	// todo: add your logic here and delete this line
-	_, err := utils.VerifyToken(in.Token)
+
+	// 查看token是否注销
+	get, err := l.svcCtx.Rds.Get(fmt.Sprintf("%s%s", model.BlackList_AccessToken, in.Token))
+	if err != nil {
+		return nil, err
+	}
+	if get != "" {
+		return &auth.VerifyResp{
+			Res: false,
+			Msg: "token失效",
+		}, nil
+	}
+
+	claims, err := utils.VerifyToken(in.Token)
 	if err != nil {
 		return nil, err
 	}
 
 	return &auth.VerifyResp{
 		Res: true,
+		Claims: &structpb.Struct{Fields: map[string]*structpb.Value{
+			"user_id":     {Kind: &structpb.Value_NumberValue{NumberValue: float64(claims.UserId)}},
+			"refresh":     {Kind: &structpb.Value_StringValue{StringValue: claims.RefreshToken}},
+			"refresh_exp": {Kind: &structpb.Value_NumberValue{NumberValue: float64(claims.RefreshExpires)}},
+		}},
 	}, nil
 }
